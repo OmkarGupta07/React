001c176351799a023d7dda77c80ce5338cafdc8b
OmkarGupta07 <guptaomkar91@gmail.com>
2024-10-24 00:03:17 +0530

    Episode-2

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 // 1st argument takes name of the tag  which we want to create 2 is to specify attributes such as  classname,id,events such as onclick etc , 3 is to display on screen
 const h1tag=React.createElement('h1',{id:'heading',onClick:()=>{alert('heyy every onr')}}, "hello boii") //creates element 
 
@@ -18,11 +22,18 @@ React.createElement("div",{id:"parent"},
 ));
 
 
-
 console.log(parent);
 
 
 
 
 
-root.render(parent);
+//NPM package manager it is also know as dependencies
+//package.json is configuration for npm it keeps track of version which are installed in it 
+//App cana i have 2 type of packages dev dependency other is normal dependency 
+//^ is know as carat it help us to automatically minor upgrade to latest version & ~ install major upgrade
+// package-lock.json keeps  tracks of exact version of all the package 
+// every dependencies has it own package.json and other dependency
+//node modules are collection of dependencies we dont npde_modules on git because we can re genrate through npm i 
+//npx means executing the package 
+//Parcel  -Dev Build,Local Server , Hot module replacement , File watching algorithm , caching for faster builds , Image optimization , minification , bundling , compressing , differential bundling - support older browser , error handling , remove unused code , https
